352cb70e2e75be167a163985d790fedc05178400
pazgomezcristallizeth-eng <pazgomezcristallizeth@cetis50cdmx.com>
2025-09-22 20:06:26 -0600

    siddhartha

---
+++ proyectoparcial/Galeria/parcial/src/main.py
@@ -0,0 +1,16 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title="Siddhartha"
+    page.bgcolor=ft.Colors.YELLOW_50
+    
+    Siddhartha = (biografia)
+    {
+            "titulo": "Why You?",
+            "lanzamiento": "2008",
+            "descripcion": "Es catalogado como una de las mejores producciones del año por las revistas especializadas Warp, Marvin, Rolling Stone, Sonika y Círculo Mixup.",
+            "imagen": "https://raw.githubusercontent.com/pazgomezcristallizeth-eng/proyecto-parcial/refs/heads/main/Why%20You.jpeg"
+        }
+    
+ft.app(main)
